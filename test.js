2778a6b389e1a8a77c3692ef1f0b71115d11a636
drylikov <denis.rylikov@protonmail.com>
2025-05-05 11:50:21 +0300

    node_async_simple
    Multiply two numbers, slowly, on the thread pool.

---
+++ test.js
@@ -0,0 +1,11 @@
+var nas = require("./build/default/nas")
+  , start = Date.now()
+
+console.log("js "+(Date.now() - start), "before hello")
+nas.doSomething(1, 2, "hello", function (er, res, n) {
+  console.log("js "+(Date.now() - start), er, res, n)
+})
+console.log("js "+(Date.now() - start), "before goodbye")
+nas.doSomething(3, 4, "goodbye", function (er, res, n) {
+  console.log("js "+(Date.now() - start), er, res, n)
+})
